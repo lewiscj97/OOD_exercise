94d639c7fdb4e8022c18bc664b85c6bb5496809a
Alice Lieutier <alice.lieutier@gmail.com>
2021-09-29 18:10:39 +0100

    refactor

--- lib/league_organiser.rb
+++ lib/league_organiser.rb
@@ -1,24 +1,13 @@
 class LeagueOrganiser
-  def initialize
-    @players = []
-    @has_started = false
-    @matches = []
+
+  def initialize(players)
+    raise "A league needs at least two players." if players.length < 2
+    @players = players
+    @matches = generate_matches(@players)
     @winners = {}
   end
 
-  def add_player(name)
-    raise "Can't add players once the league has started." if @has_started
-    @players << name
-  end
-
-  def start_league
-    raise "The league needs at least two players to start." if @players.length < 2
-    @has_started = true
-    @matches = generate_matches(@players)
-  end
-
   def show_matches
-    raise "Cannot show matches: League hasn't started yet." if !@has_started
     matches_strings = @matches.map.with_index do | players, index |
       player_1, player_2 = players
       string = "Match #{index + 1}: #{player_1} vs #{player_2}"
@@ -30,14 +19,14 @@ class LeagueOrganiser
     puts matches_strings.join("\n")
   end
 
-  def record_win(match_id, winner)
+  def record_a_win(match_id, winner)
     raise "Wrong match number: #{match_id}." if match_id > @matches.length
     raise "Wrong player name: #{winner}." if !@matches[match_id - 1].include? winner
     @winners[match_id] = winner
   end
 
   def overall_winner
-    raise "Cannot determine winner: League hasn't ended yet." if @winners.length < @matches.length
+    raise "Cannot determine winner: Some matches do not have a winner yet." if @winners.length < @matches.length
     return calculate_overall_winner
   end
 
@@ -49,12 +38,12 @@ class LeagueOrganiser
 
   # buggy if more than one player has the same number of win
   def calculate_overall_winner
-    nb_of_wins_per_player = {}
+    number_of_wins_per_player = {}
     @players.each do | name |
-      nb_of_wins_per_player[name] = @winners.values.count(name)
+      number_of_wins_per_player[name] = @winners.values.count(name)
     end
-    max_num_of_win = nb_of_wins_per_player.values.max
-    return nb_of_wins_per_player.key(max_num_of_win)
+    max_number_of_win = number_of_wins_per_player.values.max
+    return number_of_wins_per_player.key(max_number_of_win)
   end
 
 end
--- spec/league_organiser_spec.rb
+++ spec/league_organiser_spec.rb
@@ -1,35 +1,25 @@
 require 'league_organiser'
 
-describe 'league_organiser' do
+describe 'LeagueOrganiser' do
 
   describe 'work in happy scenarios' do
     it 'calculates the winner correctly' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      league_organiser.start_league
+      league = LeagueOrganiser.new(["Alice", "Bob", "Charlie"])
         # Match 1: Alice vs Bob
         # Match 2: Alice vs Charlie
         # Match 3: Bob vs Charlie
-      league_organiser.record_win(1, "Alice")
-      league_organiser.record_win(2, "Charlie")
-      league_organiser.record_win(3, "Charlie")
-      expect(league_organiser.overall_winner).to eq "Charlie"
+      league.record_a_win(1, "Alice")
+      league.record_a_win(2, "Charlie")
+      league.record_a_win(3, "Charlie")
+      expect(league.overall_winner).to eq "Charlie"
     end
   
     it 'displays matches as expected' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      league_organiser.add_player("Denise")
-      league_organiser.add_player("Emily")
-      league_organiser.start_league
-      league_organiser.record_win(1, "Alice")
-      league_organiser.record_win(2, "Charlie")
-      league_organiser.record_win(6, "Bob")
-      expect{league_organiser.show_matches}.to output(
+      league = LeagueOrganiser.new(["Alice", "Bob", "Charlie", "Denise", "Emily"])
+      league.record_a_win(1, "Alice")
+      league.record_a_win(2, "Charlie")
+      league.record_a_win(6, "Bob")
+      expect{league.show_matches}.to output(
 <<-matches
 Match 1: Alice vs Bob, Alice wins
 Match 2: Alice vs Charlie, Charlie wins
@@ -48,60 +38,30 @@ matches
 
   describe 'correctly errors in unhappy scenarios' do
     it 'throws an error if trying to record a win with wrong Player' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      league_organiser.start_league
+      league = LeagueOrganiser.new(["Alice", "Bob", "Charlie"])
         # Match 1: Alice vs Bob
         # Match 2: Alice vs Charlie
         # Match 3: Bob vs Charlie
-      expect{league_organiser.record_win(1, "Charlie")}.to raise_error("Wrong player name: Charlie.")
+      expect{league.record_a_win(1, "Charlie")}.to raise_error("Wrong player name: Charlie.")
     end
 
     it 'throws an error if trying to record a win with wrong id' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      league_organiser.start_league
+      league = LeagueOrganiser.new(["Alice", "Bob", "Charlie"])
         # Match 1: Alice vs Bob
         # Match 2: Alice vs Charlie
         # Match 3: Bob vs Charlie
-      expect{league_organiser.record_win(4, "Charlie")}.to raise_error("Wrong match number: 4.")
+      expect{league.record_a_win(4, "Charlie")}.to raise_error("Wrong match number: 4.")
     end
 
-    it 'throws an error if trying to add a player after the league has started' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.start_league
-      expect{league_organiser.add_player("Charlie")}.to raise_error("Can't add players once the league has started.")
-    end
-
-    it 'throws an error if trying to start the league with only one player' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      expect{league_organiser.start_league}.to raise_error("The league needs at least two players to start.")
-    end
-
-    it 'throws an error if trying to show matches before the league has started' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      expect{league_organiser.show_matches}.to raise_error("Cannot show matches: League hasn't started yet.")
+    it 'throws an error if trying to create a league with only one player' do
+      expect{LeagueOrganiser.new(["Alice"])}.to raise_error("A league needs at least two players.")
     end
 
     it 'throws an error if trying to get the winner before all the matches have been played' do
-      league_organiser = LeagueOrganiser.new
-      league_organiser.add_player("Alice")
-      league_organiser.add_player("Bob")
-      league_organiser.add_player("Charlie")
-      league_organiser.start_league
-      league_organiser.record_win(1, "Alice")
-      league_organiser.record_win(2, "Charlie")
-      expect{league_organiser.overall_winner}.to raise_error("Cannot determine winner: League hasn't ended yet.")
+      league = LeagueOrganiser.new(["Alice", "Bob", "Charlie"])
+      league.record_a_win(1, "Alice")
+      league.record_a_win(2, "Charlie")
+      expect{league.overall_winner}.to raise_error("Cannot determine winner: Some matches do not have a winner yet.")
     end
   end
 end
